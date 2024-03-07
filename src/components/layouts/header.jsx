53ad28646100f3a49526989bd9bef62c9f462dd5
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-07 16:26:03 +0100

    chore(): update header and add it to the layout component

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,1 +1,17 @@
-export const Header = () => <></>
+import { Logo } from "@/components/Logo"
+import { Link } from "@/components/ui/link"
+
+const LINKS = []
+
+export const Header = () => (
+	<header>
+		<Logo />
+		<nav>
+			<div className="flex items-center gap-3">
+				{LINKS.map(link => (
+					<Link key={link.href} {...link} />
+				))}
+			</div>
+		</nav>
+	</header>
+)
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,8 @@
+import { Header } from "@/components/layouts"
+
 export const MainLayout = ({ children }) => (
 	<div className="h-screen flex flex-col">
+		<Header />
 		<main>{children}</main>
 	</div>
 )
