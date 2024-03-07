5d2c078a1170c19c569ce46d90f58d2cc999abb2
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-07 19:44:25 +0100

    chore(): update style header

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -4,7 +4,7 @@ import { Link } from "@/components/ui/link"
 const LINKS = []
 
 export const Header = () => (
-	<header>
+	<header className="w-full bg-blue-500 p-4">
 		<Logo />
 		<nav>
 			<div className="flex items-center gap-3">
