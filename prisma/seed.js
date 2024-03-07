5fc87f765ae91540cc9bfdad0e33bbb1e092512c
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-07 14:40:08 +0100

    chore(): create seed script with faker-js

---
+++ prisma/seed.js
@@ -0,0 +1,38 @@
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+const { faker } = require("@faker-js/faker")
+
+async function main() {
+	const data = []
+
+	for (let i = 0; i < 10; i += 1) {
+		data.push({
+			type: faker.helpers.arrayElement([
+				"bar",
+				"museum_pay",
+				"museum_free",
+				"restaurant",
+				"monument",
+			]),
+			name: faker.company.name(),
+			zipcode: faker.location.zipCode(),
+			country: faker.location.country(),
+			city: faker.location.city(),
+		})
+	}
+
+	await prisma.place.createMany({
+		data,
+	})
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		// eslint-disable-next-line no-console
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
