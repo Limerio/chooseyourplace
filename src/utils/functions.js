1ec14be0ec27fddd82a22a57af284418845744fd
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-08 09:36:47 +0100

    chore(): remove default api route and create handlerApi function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,6 +1,15 @@
+import { PrismaClient } from "@prisma/client"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
 	return twMerge(clsx(inputs))
+}
+
+export const handlerApi = handle => (req, res) => {
+	const prisma = new PrismaClient()
+
+	req.prisma = prisma
+
+	return handle(req, res)
 }
