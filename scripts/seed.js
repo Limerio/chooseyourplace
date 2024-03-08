d995e0b01c68a0437fb52100492320ccc70a2ff4
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-08 11:26:27 +0100

    chore(): update seed script

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,38 +1,36 @@
-const { PrismaClient } = require("@prisma/client")
-const prisma = new PrismaClient()
+require("dotenv").config()
 const { faker } = require("@faker-js/faker")
+const { MongoClient } = require("mongodb")
 
 async function main() {
-	const data = []
+	const uri = process.env.DATABASE_URL
+	const client = new MongoClient(uri)
 
-	for (let i = 0; i < 10; i += 1) {
-		data.push({
-			type: faker.helpers.arrayElement([
-				"bar",
-				"museum_pay",
-				"museum_free",
-				"restaurant",
-				"monument",
-			]),
-			name: faker.company.name(),
-			zipcode: parseInt(faker.location.zipCode(), 10),
-			country: faker.location.country(),
-			city: faker.location.city(),
-		})
+	try {
+		await client.connect()
+		const database = client.db(uri.split("/").at(-1))
+		const places = database.collection("places")
+		const data = []
+
+		for (let i = 0; i < 10; i += 1) {
+			data.push({
+				type: faker.helpers.arrayElement([
+					"bar",
+					"museum_pay",
+					"museum_free",
+					"restaurant",
+					"monument",
+				]),
+				name: faker.company.name(),
+				zipcode: parseInt(faker.location.zipCode(), 10),
+				country: faker.location.country(),
+				city: faker.location.city(),
+			})
+		}
+		await places.insertMany(data)
+	} finally {
+		await client.close()
 	}
-
-	await prisma.place.createMany({
-		data,
-	})
 }
 
 main()
-	.then(async () => {
-		await prisma.$disconnect()
-	})
-	.catch(async e => {
-		// eslint-disable-next-line no-console
-		console.error(e)
-		await prisma.$disconnect()
-		process.exit(1)
-	})
