23a201359673107671491d84244193780dc7fce7
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-10 17:47:26 +0100

    chore(): update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button"
+import { DataTable } from "@/components/ui/data-table"
 import { requestAPI } from "@/utils/functions"
 import { QueryClient, dehydrate, useQuery } from "@tanstack/react-query"
+import { ArrowUpDown } from "lucide-react"
 
 export const getServerSideProps = async () => {
 	const queryClient = new QueryClient()
@@ -15,14 +18,55 @@ export const getServerSideProps = async () => {
 		},
 	}
 }
+const columns = [
+	{
+		accessorKey: "building",
+		header: "Building",
+	},
+	{
+		accessorKey: "name",
+		header: ({ column }) => (
+			<Button
+				variant="ghost"
+				onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+			>
+				Name
+				<ArrowUpDown className="ml-2 h-4 w-4" />
+			</Button>
+		),
+	},
+	{
+		accessorKey: "city",
+		header: "city",
+	},
+	{
+		accessorKey: "zipcode",
+		header: "Zip Code",
+	},
+	{
+		accessorKey: "country",
+		header: "Country",
+	},
+]
 
 export default function Home() {
-	const { data } = useQuery({
+	const { data, isLoading, isError } = useQuery({
 		queryKey: ["locations"],
 		queryFn: () => requestAPI(false, "/locations"),
 	})
 
+	if (isLoading) {
+		return <div className="bg-slate-500">Loading...</div>
+	}
+
+	if (isError) {
+		return <div className="bg-red-600">Error</div>
+	}
+
 	return (
-		<div className="text-black dark:text-white">{JSON.stringify(data)}</div>
+		<div className="container mx-auto py-10">
+			<h1 className="text-6xl text-center">List of places</h1>
+			<DataTable filterInput={"name"} columns={columns} data={data} />
+		</div>
 	)
 }
