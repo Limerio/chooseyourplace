2d0849a91de696d2ed5a93cba908d8d6b2dddf12
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-12 14:11:03 +0100

    chore(): create final step component and add method POST for the process of creation of places

--- src/components/forms/places/FinalStep.jsx
+++ src/components/forms/places/FinalStep.jsx
@@ -1,1 +1,74 @@
-export const FinalStep = () => <h1>Final step</h1>
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { useMultiStepsForm } from "@/hooks/forms"
+import { capitalize, requestAPI } from "@/utils/functions"
+import { useRouter } from "next/router"
+
+const finalStepCards = ["Information about the building", "Details"]
+
+// eslint-disable-next-line max-lines-per-function
+export const FinalStep = () => {
+	const { formsData, back } = useMultiStepsForm()
+	const router = useRouter()
+	const finishStepClick = () => async () => {
+		const body = {
+			info: formsData[0].data,
+			details: formsData[1].data,
+		}
+
+		await requestAPI(false, "/places", {
+			method: "POST",
+			body: JSON.stringify(body),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+		router.push("/")
+	}
+
+	return (
+		<>
+			<div className="flex gap-2">
+				{finalStepCards.map((title, i) => (
+					<Card className="w-full" key={title}>
+						<CardHeader>
+							<CardTitle className="text-2xl">{title}</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<div className="flex flex-wrap gap-20">
+								{Object.keys(formsData[i].data).map(formDataKey => {
+									const value = formsData[i].data[formDataKey]
+
+									return (
+										<DetailsForm
+											title={formDataKey}
+											key={formDataKey}
+											value={
+												typeof value === "string" ? capitalize(value) : value
+											}
+										/>
+									)
+								})}
+							</div>
+						</CardContent>
+					</Card>
+				))}
+			</div>
+
+			<div className="flex items-center gap-2">
+				<Button className="w-full" onClick={() => back()}>
+					Previous
+				</Button>
+				<Button className="w-full" onClick={finishStepClick()}>
+					Finish
+				</Button>
+			</div>
+		</>
+	)
+}
+const DetailsForm = ({ title, value }) => (
+	<div className="flex flex-col space-y-1.5">
+		<h3 className="text-xl font-bold">{capitalize(title)}</h3>
+		<p>{value}</p>
+	</div>
+)
