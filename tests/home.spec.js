5c98193d94417932201a57dbfd1ed52285cb5891
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-20 16:11:23 +0100

    chore(): create first e2e test

---
+++ tests/home.spec.js
@@ -0,0 +1,31 @@
+import { expect, test } from "@playwright/test"
+
+test.beforeEach(async ({ page }) => {
+	await page.goto("http://localhost:3000/")
+})
+
+test("Theme", async ({ page }) => {
+	await page.getByRole("button", { name: "Toggle theme" }).click()
+	await page.getByRole("menuitem", { name: "Light" }).click()
+	await expect(page.locator("html")).toHaveClass("light")
+
+	await page.getByRole("button", { name: "Toggle theme" }).click()
+	await page.getByRole("menuitem", { name: "Dark" }).click()
+	await expect(page.locator("html")).toHaveClass("dark")
+
+	await page.getByRole("button", { name: "Toggle theme" }).click()
+	await page.getByRole("menuitem", { name: "System" }).click()
+})
+
+test("Search by name", async ({ page }) => {
+	await page.getByPlaceholder("Search by name...").click()
+})
+
+test("Pagination", async ({ page }) => {
+	await expect(page.getByRole("button", { name: "Previous" })).toBeDisabled()
+	await page.getByRole("button", { name: "Next" }).click()
+
+	await expect(page.getByRole("button", { name: "Previous" })).toBeEnabled()
+	await page.getByRole("button", { name: "Previous" }).click()
+	await expect(page.getByRole("button", { name: "Previous" })).toBeDisabled()
+})
