f834bbe3a99af16b367d46fe1415a7eb992a97c6
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-21 22:40:08 +0100

    chore(): update vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,12 @@
+import react from "@vitejs/plugin-react"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+	plugins: [react()],
 	test: {
+		alias: {
+			"@/": new URL("./src/", import.meta.url).pathname,
+		},
 		exclude: ["tests", "node_modules"],
 		globals: true,
 		environment: "jsdom",
