ea62001f895a5bc159f425761779cfc6821527e3
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-24 19:14:49 +0100

    chore(): create logo test

--- src/components/layouts/Logo/Logo.jsx
+++ src/components/layouts/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import { Plane } from "lucide-react"
 export const Logo = () => (
 	<div className="flex items-center gap-2">
 		<Avatar>
-			<Plane size={28} />
+			<Plane size={28} title="plane-icon" />
 		</Avatar>
 		<span className="font-bold select-none text-lg">Chooseyourplace</span>
 	</div>
