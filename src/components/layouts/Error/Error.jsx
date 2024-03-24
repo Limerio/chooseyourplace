d53b64ed377a5f9e4b5e6255507495b65dd9ff4f
Limerio <44976231+Limerio@users.noreply.github.com>
2024-03-24 19:14:06 +0100

    chore(): create Errorhandler test

--- src/components/layouts/Error/Error.jsx
+++ src/components/layouts/Error/Error.jsx
@@ -1,7 +1,7 @@
 import { useTranslations } from "next-intl"
 
 export const ErrorHandler = ({ isError, children }) => {
-	const t = useTranslations("Error")
+	const t = useTranslations("ErrorHandler")
 
 	if (isError) {
 		return <div className="bg-red-600 rounded p-4">{t("text")}</div>
@@ -10,4 +10,4 @@ export const ErrorHandler = ({ isError, children }) => {
 	return children
 }
 
-ErrorHandler.messages = ["Error"]
+ErrorHandler.messages = ["ErrorHandler"]
