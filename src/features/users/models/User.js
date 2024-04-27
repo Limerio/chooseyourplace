f90d6a59411aabebe2798ccb920612c5117385c2
Limerio <44976231+Limerio@users.noreply.github.com>
2024-04-28 01:07:43 +0200

    chore(): moove user models and schema to an subfolder database

--- src/features/users/models/User.js
+++
@@ -1,5 +0,0 @@
-import { UserSchema } from "@/features/places/schemas"
-import mongoose from "mongoose"
-
-export const PlaceModel =
-	mongoose.models.Place || mongoose.model("User", UserSchema, "users")
